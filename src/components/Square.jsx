964cdb47c9339045c5ee97a4010c052919df24f6
Yuri Mello <yuridemeello@gmail.com>
2019-05-31 22:50:29 -0300

    função next player

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -4,9 +4,9 @@ function Square(props) {
     return ( 
         <button 
             className="square" 
-            onClick={() => this.props.onClick()}
+            onClick={() => props.onClick()}
         >
-            {this.props.value}
+            {props.value}
         </button>
     );
 }
